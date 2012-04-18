dc99691d978894caa759b24e51c3d39f04b0152d
egaburov@darkstar <egaburov.work@gmail.com>
2012-04-18 15:18:21 -0500

    added Density estimator with an example. It runs, but still need more tests

---
+++ runtime/density_estimator/Particle.h
@@ -0,0 +1,103 @@
+#ifndef __PARTICLE_H__
+#define __PARTICLE_H__
+
+#include <vector>
+#include "vector3.h"
+#include "morton_key.h"
+
+class Particle
+{
+  public:
+ 
+    typedef std::vector<Particle> Vector;
+    typedef Vector::      iterator      Iterator;
+    typedef Vector::const_iterator constIterator;
+
+    int ID;                     // 1 1
+    vec3 pos;                   // 3 4
+    float mass;                 // 1 5
+
+  protected:
+    float h;  /* range */       // 1 6
+
+  public:
+    float density;              // 1 7
+    int   nnb;                  // 1 8
+    morton_key<vec3, float> key; // 2 10
+
+  protected:
+    float hinv;                  // 1 11
+    int iPad[5];
+
+  public:
+
+    /********* methods ******/
+
+    Particle(const int _ID, const vec3 &_pos, const float _mass, const float _h = 0.0f) :
+      ID(_ID), pos(_pos), mass(_mass), h(_h), density(0.0f), nnb(0) {  assert(sizeof(Particle) == 16*sizeof(float)); }
+
+    void set_h(const float _h) 
+    {
+      h    = _h;
+      hinv = 1.0f/h;
+    }
+    float get_h() const {return h;}
+    float get_hinv() const {return hinv;}
+
+    void compute_key(const vec3 &origin, const float size) 
+    {
+      key = morton_key<vec3, float>(pos - origin, size);
+    };
+
+    int octkey(const int rshift)
+    {
+      return 7 & (key.val >> rshift);
+    }
+
+#if 0 /* does not work w/ OpenMP, not sure why ... */
+    const Particle operator = (const Particle &rhs)
+    {
+      typedef float v4sf __attribute__ ((vector_size(16)));
+      v4sf *lp =(v4sf *)this;
+      v4sf *rp =(v4sf *)(&rhs);
+      lp[0] = rp[0];
+      lp[1] = rp[1];
+      lp[2] = rp[2];
+      lp[3] = rp[3];
+      
+      return *this;
+    }
+#endif
+};
+
+
+#if 0
+namespace std
+{
+	template <> 
+	inline void iter_swap <Particle::Iterator, Particle::Iterator> (Particle::Iterator a, Particle::Iterator b)
+  {
+		typedef float v4sf __attribute__ ((vector_size(16)));
+		v4sf *ap =(v4sf *)&(*a);
+		v4sf *bp =(v4sf *)&(*b);
+		v4sf tmpa0 = ap[0];
+		v4sf tmpa1 = ap[1];
+		v4sf tmpa2 = ap[2];
+		v4sf tmpa3 = ap[3];
+		v4sf tmpb0 = bp[0];
+		v4sf tmpb1 = bp[1];
+		v4sf tmpb2 = bp[2];
+		v4sf tmpb3 = bp[3];
+		ap[0] = tmpb0;
+		ap[1] = tmpb1;
+		ap[2] = tmpb2;
+		ap[3] = tmpb3;
+		bp[0] = tmpa0;
+		bp[1] = tmpa1;
+		bp[2] = tmpa2;
+		bp[3] = tmpa3;
+	}
+}
+#endif
+
+#endif /* __PARTICLE_H__ */
---
+++ runtime/density_estimator/density.h
@@ -0,0 +1,103 @@
+#ifndef __DENSITY_H__
+#define __DENSITY_H__
+
+#include <algorithm>
+#include "Node.h"
+#include "wtime.h"
+
+
+#if 0
+#define SLOW
+#endif
+
+struct Density
+{
+  typedef boundary<float> Boundary;
+
+  Particle::Vector density;
+  Boundary BBox;  /* bounding box */
+  
+  struct cmp_particle_key 
+  {
+    bool operator () (const Particle &a, const Particle &b)
+    {
+      return a.key.val < b.key.val;
+    }
+  };
+
+  struct cmp_particle_ID
+  {
+    bool operator () (const Particle &a, const Particle &b)
+    {
+      return a.ID < b.ID;
+    }
+  };
+
+  Density(const Particle::Vector &ptcl_in, const int Nuse)
+  {
+    const double t0 = wtime();
+
+    std::vector<Particle> &ptcl = Node::ptcl;
+    ptcl.reserve(Nuse);
+    const int Nin = ptcl_in.size();
+    const float fac = std::min(1.0f, (float)Nin/(float)Nuse);
+
+
+    /* import particles and compute the Bounding Box */
+    for (int i = 0; i < Nuse; i++)
+    {
+      ptcl.push_back(ptcl_in[(int)(i * fac)]);
+      BBox.merge(Boundary(ptcl.back().pos));
+    }
+    std::cerr << BBox.min << std::endl;
+    std::cerr << BBox.max << std::endl;
+    const vec3  vsize = BBox.hlen();
+    const float rsize = std::max(vsize.x, std::max(vsize.x, vsize.y)) * 2.0f;
+
+    /* now build the tree */
+
+    const int nbody = Nuse;
+
+    for (int i = 0; i < nbody; i++)
+      ptcl[i].compute_key(BBox.min, rsize);
+
+    std::sort(ptcl.begin(), ptcl.end(), cmp_particle_key());
+
+    Node::Node_heap.push_back(Node());
+    Node &root = Node::Node_heap[0];
+    for (int i = 0; i < nbody; i++)
+      root.push_particle(i, 60);
+
+#if 1  /* if h's are not know this set-up estimated range */
+    const float volume = rsize*rsize*rsize;
+		root.set_init_h(float(32), volume);
+#endif
+    root.make_boundary();
+
+#ifdef SLOW
+
+#pragma omp parallel for
+	for(int i=0; i<nbody; i++)
+		ptcl[i] << root;
+#else /* FAST */
+
+#ifdef _OPENMP
+	std::vector<Node *> group_list;
+	root.find_group_Node(2000, group_list);
+#pragma omp parallel for schedule(dynamic)
+	for(int i=0; i<(int)group_list.size(); i++)
+		*group_list[i] << root;
+#else
+	root << root;
+#endif /* _OPENMP */
+
+#endif /* SLOW */
+
+  const double t1 = wtime();
+  fprintf(stderr, " -- Density done in %g sec \n",  t1 - t0);
+
+  };
+};
+
+
+#endif /* __DENSITY_H__ */
