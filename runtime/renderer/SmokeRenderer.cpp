c2ad5ae39df39fa26fa71e78d68c2ccfa333e45a
egaburov <egaburov.work@gmail.com>
2012-05-14 21:41:10 +0200

    restored Hubble Deep Field and sped up rotation

--- runtime/renderer/SmokeRenderer.cpp
+++ runtime/renderer/SmokeRenderer.cpp
@@ -151,9 +151,9 @@ SmokeRenderer::SmokeRenderer(int numParticles, int maxParticles) :
 	glGenTextures(1, &mPosBufferTexture);
 	m_noiseTex = createNoiseTexture(64, 64, 64);
 
-    m_cubemapTex = loadCubemapCross("../images/Carina_cross.ppm");
+//    m_cubemapTex = loadCubemapCross("../images/Carina_cross.ppm");
     //m_cubemapTex = loadCubemap("../images/deepfield%d.ppm");
-    //m_cubemapTex = loadCubemap("../images/deepfield%d_1k.ppm");
+	m_cubemapTex = loadCubemap("../images/deepfield%d_1k.ppm");
 
 	m_spriteTex = createSpriteTexture(256);
 
@@ -768,13 +768,13 @@ void SmokeRenderer::drawSlices()
     glClearColor(0.0, 0.0, 0.0, 0.0); 
     glClear(GL_COLOR_BUFFER_BIT);
 
-    #if 0
+    #if 1
         glMatrixMode(GL_MODELVIEW);
 	      glPushMatrix();
 	      glLoadIdentity();
         static float rotate = 0;
         glRotatef(rotate, 1,0,0);
-        rotate += 0.05f;
+        rotate += 0.1f;
         drawSkybox(m_cubemapTex);
         glPopMatrix();
     #else
