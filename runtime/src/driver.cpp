8f8fc55e53c3d8a44908a0ca10ec7debf2394367
evghenii <egaburov@dds.nl>
2014-09-24 20:14:11 +0200

    +1

--- runtime/src/driver.cpp
+++ runtime/src/driver.cpp
@@ -1,13 +1,10 @@
 #include <mpi.h>
 #include <dlfcn.h>
-#include <cstdio>
-#include <cstdlib>
 #include <vector>
 #include <string>
+#include <sstream>
 #include <cassert>
 #include <functional>
-#include <unistd.h>
-
 
 class DynamicLoader
 {
@@ -15,80 +12,101 @@ class DynamicLoader
     void* const m_handle;
 
   public:
-    DynamicLoader(std::string const& filename) :
+    DynamicLoader(const std::string &filename) :
       m_handle(dlopen(filename.c_str(), RTLD_LAZY)) 
-    {
-      if (!m_handle)
-      {
-        throw std::logic_error("can't load library named \"" + filename + "\"");
-      }
-    }
+  {
+    if (!m_handle)
+      throw std::logic_error("can't load library named \"" + filename + "\"");
+  }
 
-    template<class T> std::function<T> load(std::string const& functionName) const
+    template<class T> std::function<T> load(const std::string &functionName) const
     {
       dlerror();
-      void* const result = dlsym(m_handle, functionName.c_str());
+      const void* result = dlsym(m_handle, functionName.c_str());
       if (!result)
       {
-        char* const error = dlerror();
+        const char* error = dlerror();
         if (error)
-        {
           throw std::logic_error("can't find symbol named \"" + functionName + "\": " + error);
-        }
       }
 
       return reinterpret_cast<T*>(result);
     }
-
 };
+
+
+static std::vector<std::string> lSplitString(const std::string &s, char delim)
+{
+  std::vector<std::string> elems;
+  std::stringstream ss(s);
+  std::string item;
+  while (std::getline(ss, item, delim)) 
+    if (!item.empty())
+      elems.push_back(item);
+  return elems;
+}
+
+std::vector<std::vector<std::string>> parseInput()
+{
+  std::string input;
+  std::string tmp;
+  while(std::getline(std::cin, tmp)) 
+    input += tmp + "\n";
+
+  int size[2] = {static_cast<int>(input.size()), static_cast<int>(input.capacity())};
+  MPI_Bcast(&size, 2, MPI_INT, 0, MPI_COMM_WORLD);
+  input.resize (size[0]);
+  input.reserve(size[1]);
+  MPI_Bcast(&input[0], size[0], MPI_BYTE, 0, MPI_COMM_WORLD);
+
+  const std::vector<std::string> lines = lSplitString(input,'\n');
+  std::vector<std::vector<std::string>> programs;
+  for (const auto &line : lines)
+    programs.push_back(lSplitString(line,' '));
+  return programs;
+}
 
 int main(int argc, char *argv[]) 
 {
   MPI_Init(&argc, &argv);
 
-  auto splitComm = [&](const int nway)
-  {
-    MPI_Comm comm;
-    int rank, nrank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &nrank);
-    const int color = rank%nway;
-    const int key   = rank/nway;
-    MPI_Comm_split(MPI_COMM_WORLD,color,key,&comm);
-    return std::make_tuple(comm,color,key);
-  };
+  int rank, nrank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &nrank);
 
-  const auto commData = splitComm(2);  
-  const auto comm  = std::get<0>(commData);
-  const auto color = std::get<1>(commData);
+  const auto &programs = parseInput();
 
-  auto getCwd = []()  
+  if (rank == 0)
   {
-    char cwd[FILENAME_MAX];
-    assert(getcwd(cwd, sizeof(cwd)) > 0);
-    return std::string(cwd);
-  };
-  const auto cwd = getCwd();
-
-  std::vector<std::string> arguments;
-  std::string libName(cwd);
-  libName += "/";
-  if (color == 0)
-  {
-    arguments.push_back("app1");
-    arguments.push_back("arg11");
-    libName += "libapp1.so";
+    std::cerr << "Executing the following binaries: \n";
+    for (const auto &p  : programs)
+    {
+      for (const auto &arg : p)
+        std::cerr << arg << " ";
+      std::cerr << std::endl;
+    }
   }
-  else
+
+  const int nprograms = programs.size();
+  if (nrank%nprograms != 0)
   {
-    arguments.push_back("app2");
-    arguments.push_back("arg21");
-    arguments.push_back("arg22");
-    libName += "libapp2.so";
+    if (rank == 0)
+      std::cerr << "Fatal: nranks= " << nrank << " should be divisible by the nprogram= " << nprograms << ".\n";
+    MPI_Finalize();
+    ::exit(0);
   }
+
+  const int color = rank%nprograms;
+  const int key   = rank/nprograms;
+
+  MPI_Comm comm;
+  MPI_Comm_split(MPI_COMM_WORLD,color,key,&comm);
+
+  const auto &arguments = programs[color];
+  const std::string &libName = arguments[0];
 
   const DynamicLoader dll(libName);
-  const auto program = dll.load<void(int,char**,MPI_Comm)>("run");
+  const auto program = dll.load<void(int,char**,MPI_Comm)>("main");
 
   std::vector<char*> argVec;
   for (const auto &arg : arguments)
